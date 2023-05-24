f61663f3365540897f7427e29c8bd58954dac654
mohamad Mehdi Hajati <78010369+asus323@users.noreply.github.com>
2023-05-24 12:50:30 +0330

    check if product added to cart or not

---
+++ check_product_add_to_cart.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Change the add to cart text on single product pages
+ */
+add_filter('woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text');
+function woo_custom_cart_button_text() {
+	
+	foreach( WC()->cart->get_cart() as $cart_item_key => $values ) {
+		$_product = $values['data'];
+	
+		if( get_the_ID() == $_product->id ) {
+			return __('Already in cart - Add Again?', 'woocommerce');
+		}
+	}
+	
+	return __('Add to cart', 'woocommerce');
+}
+/**
+ * Change the add to cart text on product archives
+ */
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_text' );
+function woo_archive_custom_cart_button_text() {
+	
+	foreach( WC()->cart->get_cart() as $cart_item_key => $values ) {
+		$_product = $values['data'];
+	
+		if( get_the_ID() == $_product->id ) {
+			return __('Already in cart', 'woocommerce');
+		}
+	}
+	
+	return __('Add to cart', 'woocommerce');
+}
