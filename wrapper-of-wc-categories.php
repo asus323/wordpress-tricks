946d81ae5f851f52af9396f8a77497fe4909b5a9
mohamad Mehdi Hajati <78010369+asus323@users.noreply.github.com>
2023-10-16 15:21:51 +0330

    Update and rename wrapper-of-wc-categories to wrapper-of-wc-categories.php

--- wrapper-of-wc-categories.php
+++ wrapper-of-wc-categories.php
@@ -1,3 +1,4 @@
+<?php 
 add_filter('woocommerce_before_output_product_categories','woocommerce_output_product_categories_custom_start');
 add_filter('woocommerce_after_output_product_categories','woocommerce_output_product_categories_custom_end');
 function woocommerce_output_product_categories_custom_start(){
